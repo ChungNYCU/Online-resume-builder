12c4e22ed1f8e9bfd55496d4b5baf95adc2d4501
Jun <jliao1@seattleu.edu>
2021-10-24 23:06:46 -0700

    Added WorkExperienceModel in WorkExperienceModel.cs

---
+++ src/Models/WorkExperienceModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class WorkExperienceModel
+    {
+        public string Id { get; set; }
+        public string CandidateId { get; set; }
+        public string Employer { get; set; }
+        public string Title { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string RoleDescription { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<WorkExperienceModel>(this);
+    }
+}
