a960298218ce66d2107032debdb0ca91806e8798
bennywongyp <ywong@seattleu.edu>
2021-10-29 15:53:54 -0700

    Adding comments in WorkExperienceModel.cs
    Adding comments in WorkExperienceModel.cs

--- src/Models/WorkExperienceModel.cs
+++ src/Models/WorkExperienceModel.cs
@@ -1,9 +1,15 @@
 ﻿using System.Text.Json;
-
+/// <summary>
+/// work experience control path
+/// allow developer to add or remove the options in resumes
+/// </summary>
 namespace ContosoCrafts.WebSite.Models
 {
+    // getting data from json
+    // allow developer to create extra options for the working experience
     public class WorkExperienceModel
     {
+        // list of working experience input from user
         public string Id { get; set; }
         public string CandidateId { get; set; }
         public string Employer { get; set; }
@@ -12,6 +18,7 @@ namespace ContosoCrafts.WebSite.Models
         public string EndDate { get; set; }
         public string RoleDescription { get; set; }
 
+        // renew the user's input or get the data from the json
         public override string ToString() => JsonSerializer.Serialize<WorkExperienceModel>(this);
     }
 }
