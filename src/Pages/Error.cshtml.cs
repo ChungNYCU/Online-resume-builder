8a8eea88139a77593dcf05ed2c6b7287862d3a26
bennywongyp <ywong@seattleu.edu>
2021-10-29 16:02:41 -0700

    Adding comment in Error.cshtml.cs
    adding comment in Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 //Xinyu added
+/// <summary>
+/// The Development environment shouldn't be enabled for deployed applications.
+/// For local debugging, enable the Development environment by
+/// setting the ASPNETCORE_ENVIRONMENT environment variable to
+/// evelopment and restarting the app.
+/// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -20,6 +26,11 @@ namespace ContosoCrafts.WebSite.Pages
             _logger = logger;
         }
 
+        /// <summary>
+        /// Gets or sets the current operation (<see cref="T:System.Diagnostics.
+        /// Activity" />) for the current thread.  This flows across async calls.
+        /// </summary>
+        /// <returns>The current operation for the current thread.</returns>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
