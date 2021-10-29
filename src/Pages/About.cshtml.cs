08a408abd4a9af43e2af550d30e94e46da5a7932
bennywongyp <ywong@seattleu.edu>
2021-10-29 15:32:31 -0700

    Removing Error comment in About.cshtml
    removing error comment in About.cshtml
    
    adding comment in About.cshml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+/// <summary>
+/// this page is showing the information about the develop
+/// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
+    // the data to show in About.cshtml
     public class AboutModel : PageModel
     {
         private readonly ILogger<AboutModel> _logger;
