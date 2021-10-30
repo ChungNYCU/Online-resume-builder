e8e845b166434d1436b14ed094e0b7f9565bc79e
bennywongyp <ywong@seattleu.edu>
2021-10-29 17:39:38 -0700

    Add comment in Privacy.cshtml.cs
    add comment in privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+/// <summary>
+/// Users control privacy, companies ensure protection. Another important
+/// distinction between privacy and protection is who is typically in control.
+/// For privacy, users can often control how much of their data is shared and
+/// with whom. For protection, it is up to the companies handling data to
+/// ensure that it remains private.
+/// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
     public class PrivacyModel : PageModel
