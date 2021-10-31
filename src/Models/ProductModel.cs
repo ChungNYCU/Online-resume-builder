5d116db6978685cda1467f2caffef8b33a9b8e0a
Wei-Che Chung <wchung@seattleu.edu>
2021-10-31 14:02:44 -0700

    Added AwardModel to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -17,15 +17,24 @@ namespace ContosoCrafts.WebSite.Models
         public string Photo { get; set; }
         public string LinkedinUrl { get; set; }
         public string FullName { get; set; }
-        public string Awards { get; set; }
+        public AwardModel Awards { get; set; }
         public string EducationHistory { get; set; }
         public string PersonalSkill { get; set; }
         public string AboutMe { get; set; }
         public int[] Ratings { get; set; }
-        
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
- 
     }
+    
+    /// <summary>
+    /// A model to storge Award information
+    /// </summary>
+    public class AwardModel
+    {
+        public string Award { get; set; }
+        public string AwardDate { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<AwardModel>(this);
+    }
+
 }
