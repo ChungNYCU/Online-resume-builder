e5dfea5fc0729c8324c7d61db52dae17b560bd4a
Xinyu Qiu <qiux1@seattleu.edu>
2021-11-01 19:47:07 -0700

    Xinyu added comments on About.cshtml.cs file so that the code can be reader for developers.

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -2,15 +2,23 @@
 using Microsoft.Extensions.Logging;
 
 /// <summary>
-/// this page is showing the information about the develop
+/// Website application introduction page 
+/// to display general information of 
+/// this web application to users
+/// along with all developers' information
 /// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
     // the data to show in About.cshtml
     public class AboutModel : PageModel
     {
+        // logger
         private readonly ILogger<AboutModel> _logger;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="logger"></param>
         public AboutModel(ILogger<AboutModel> logger)
         {
             _logger = logger;
