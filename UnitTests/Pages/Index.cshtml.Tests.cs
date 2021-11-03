33a78a549af100367810644ff38add58f41e326d
Bart Joseph Nadala <nadalab@seattleu.edu>
2021-11-02 21:25:01 -0700

    Added comments in Index.cshtml.Tests.cs

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -1,38 +1,44 @@
 using System.Linq;
-
 using Microsoft.Extensions.Logging;
-
 using Moq;
-
 using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Pages;
 
+/// <summary>
 /// Establish Unit Test for all functions on the Index Page
+/// </summary>
 namespace UnitTests.Pages.Index
 {
+    /// <summary>
+    /// Establish Unit Test for all functions on the Index Page
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
-
+        // PageModel to be tested
         public static IndexModel pageModel;
 
+        /// <summary>
+        /// Initializes all tests to be conducted
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // Logger
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
 
             pageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
             {
             };
         }
-
         #endregion TestSetup
 
         #region OnGet
         [Test]
+        /// <summary>
         /// This unit test will ensure the OnGet on the Index Page
         /// will return the searching product/resume information
+        /// </summary>
         public void OnGet_Valid_Should_Return_Products()
         {
             // Arrange
