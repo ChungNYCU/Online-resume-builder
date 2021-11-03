bf2dd9d94f297a603bf000ab2bafd486d1803aad
Bart Joseph Nadala <nadalab@seattleu.edu>
2021-11-02 21:27:07 -0700

    Added comments in Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -1,23 +1,29 @@
 using Microsoft.Extensions.Logging;
-
 using NUnit.Framework;
-
 using Moq;
-
 using ContosoCrafts.WebSite.Pages;
 
+/// <summary>
 /// Unit Tests for all functions on Privacy Page
+/// </summary>
 namespace UnitTests.Pages.Privacy
 {
+    /// <summary>
+    /// Unit Tests for all functions on Privacy Page
+    /// </summary>
     public class PrivacyTests
     {
         #region TestSetup
+        // PageModel to be tested
         public static PrivacyModel pageModel;
 
-        //To initialize the unit test for privacy page
+        /// <summary>
+        /// To initialize the unit test for privacy page
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // Logger
             var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
 
             pageModel = new PrivacyModel(MockLoggerDirect)
@@ -26,12 +32,13 @@ namespace UnitTests.Pages.Privacy
                 TempData = TestHelper.TempData,
             };
         }
-
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
         /// Curently no new function is created in the Privacy Page so no
         /// new unit testing is created yet.
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
@@ -45,7 +52,6 @@ namespace UnitTests.Pages.Privacy
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
         }
-
         #endregion OnGet
     }
 }
