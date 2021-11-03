c08540dcbeb306aa74b1279d3f082479ff5cdb6f
Bart Joseph Nadala <nadalab@seattleu.edu>
2021-11-02 20:55:04 -0700

    Added comments in Create.cshtml.Tests.cs

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -1,17 +1,24 @@
 using System.Linq;
-
 using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Pages.Product;
 
-
+/// <summary>
+/// Unit Tests for the Product.Create page
+/// </summary>
 namespace UnitTests.Pages.Product.Create
 {
+    /// <summary>
+    /// Unit Tests for the Product.Create page
+    /// </summary>
     public class CreateTests
     {
+        // Create page model for testing
         #region TestSetup
         public static CreateModel pageModel;
 
+        /// <summary>
+        /// Initialization for the tests to be made
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -22,6 +29,9 @@ namespace UnitTests.Pages.Product.Create
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Valid OnGet test that should return products
+        /// </summary>
         #region OnGet
         [Test]
         public void OnGet_Valid_Should_Return_Products()
