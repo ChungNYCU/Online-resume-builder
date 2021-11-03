acd3ad897f0de5c18795947a10859713bd02f7b7
Bart Joseph Nadala <nadalab@seattleu.edu>
2021-11-02 20:44:27 -0700

    Added comments in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
+/// <summary>
+/// Contains all parts of the website
+/// </summary>
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// The program that runs when the website is called
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Default constructor when the website is run
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             // Bulid host and run
