aaf30f21f6f0093383c7ce49d5e5f4290a8a1a81
Bart Joseph Nadala <nadalab@seattleu.edu>
2021-11-02 21:39:36 -0700

    Added comments in ProgramTest.cs

--- UnitTests/ProgramTest.cs
+++ UnitTests/ProgramTest.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Unit Tests for all Program.cs functions
+/// </summary>
 namespace UnitTests
 {
+    /// <summary>
+    /// Unit Tests for all Program.cs functions
+    /// </summary>
     class Program_Test
     {
     }
