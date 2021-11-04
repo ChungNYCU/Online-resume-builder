0e0924d893b09c07a8c71bf4564353cd5f185bd7
Wei-Che Chung <wchung@seattleu.edu>
2021-11-03 23:44:55 -0700

    Fix comments in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -11,32 +11,32 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class ProductModel
     {
-        /// getting resumes id. Id is identification for resume
+        // getting resumes id. Id is identification for resume
         public string Id { get; set; }
-        /// getting resumes status
+        // getting resumes status
         public string PersonalStatus { get; set; }
-        /// getting resume maker
+        // getting resume maker
         public string Maker { get; set; }
 
-        /// pulling resume from json
+        // pulling resume from json
         [JsonPropertyName("img")]
-        /// getting photo from ID in json
+        // getting photo from ID in json
         public string Photo { get; set; }
-        /// getting Linkedin data
+        // getting Linkedin data
         public string LinkedinUrl { get; set; }
-        /// getting user name
+        // getting user name
         public string FullName { get; set; }
-        /// getting user awards 
+        // getting user awards 
         public AwardModel Awards { get; set; }
-        /// getting resume education history 
+        // getting resume education history 
         public Education EducationHistory { get; set; }
-        /// getting resume experience 
+        // getting resume experience 
         public Experience Experiences { get; set; }
-        /// getting resume personal skill 
+        // getting resume personal skill 
         public string PersonalSkill { get; set; }
-        /// getting resume about me 
+        // getting resume about me 
         public string AboutMe { get; set; }
-        /// getting resume rating 
+        // getting resume rating 
         public int[] Ratings { get; set; }
         // get user password 
         public string Password { get; set; }
@@ -47,13 +47,12 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class AwardModel
     {
-        /// getting resume award 
+        // getting resume award 
         public string Award { get; set; }
-        /// getting award issuer
+        // getting award issuer
         public string Issuer { get; set; }
-        /// getting award date 
+        // getting award date 
         public string AwardDate { get; set; }
-
     }
 
     /// <summary>
@@ -61,21 +60,20 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class Education
     {
-        /// getting user's universitey from json
+        // getting user's universitey from json
         public string University { get; set; }
-        /// getting the university lcation from json
+        // getting the university lcation from json
         public string Location { get; set; }
-        /// getting the degree earned in university
+        // getting the degree earned in university
         public string Degree { get; set; }
-        /// getting the major studied in university
+        // getting the major studied in university
         public string Major { get; set; }
-        /// getting the GPA
+        // getting the GPA
         public string GPA { get; set; }
-        /// getting the starting date for the university
+        // getting the starting date for the university
         public string StartDate { get; set; }
-        /// getting  the ending date for the university
+        // getting  the ending date for the university
         public string EndDate { get; set; }
-
     }
 
     /// <summary>
@@ -83,17 +81,16 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class Experience
     {
-        /// getting the work title for the work experience
+        // getting the work title for the work experience
         public string Title { get; set; }
-        /// getting the employer for the work experience
+        // getting the employer for the work experience
         public string Employer { get; set; }
-        /// getting the starting date for the work experience
+        // getting the starting date for the work experience
         public string StartDate { get; set; }
-        /// getting the ending date for the work experience
+        // getting the ending date for the work experience
         public string EndDate { get; set; }
-        /// getting the description for the work experience
+        // getting the description for the work experience
         public string Description { get; set; }
-
     }
 
 }
