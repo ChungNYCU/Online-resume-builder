6852b8900de9aa2483b74dfb8d91f51bb1089823
June_windows <jliao1@seattleu.edu>
2021-11-08 04:51:14 -0800

    Write Unit Test for DeleteEducation in DeleteEducation.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Education/DeleteEducation.cshtml.Tests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Product.Education;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+namespace UnitTests.Pages.Product.Education
+{
+    class DeleteEducationTests
+    {
+        #region TestSetup
+        // AddEducation page to test
+        public static DeleteEducationModel pageModel;
+
+        /// <summary>
+        /// Initialization for the tests to be conducted
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new DeleteEducationModel(TestHelper.ProductService)
+            {
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// Valid OnGet should return an valid education record
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_An_Education_Record()
+        {
+            // Arrange
+
+            // Act
+            var Product = TestHelper.ProductService.GetAllData().First();
+            var Education = Product.EducationList[0];
+            // Act
+            pageModel.OnGet(Education.ProductID, Education.ID);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(Education.University, pageModel.Education.University);
+        }
+        #endregion OnGet
+
+
+        #region OnPost
+
+        /// <summary>
+        /// Invalid OnPost with valid model should return the Index page
+        /// </summary>
+        [Test]
+        public void OnPost_Valid_Should_Redirect_to_Index()
+        {
+            // Arrange
+            var Product = TestHelper.ProductService.GetAllData().First();
+            pageModel.Education = Product.EducationList[0];
+
+            // Act
+            // Expected result for runnning OnPost
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            //Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        /// <summary>
+        /// Invalid OnPost with invalid model should return the page
+        /// </summary>
+        [Test]
+        public void OnPost_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            // Expected result from running the OnPost
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+
+
+        #endregion OnPost
+    }
+}
