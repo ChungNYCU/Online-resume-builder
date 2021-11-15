12dcaa0826b82b3a6e221b8712e33991c3b68328
June <june.liao.bashpole@gmail.com>
2021-11-15 05:53:13 -0800

    Write Unite Test for Update Work Experience in UpdateWorkExperience.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/WorkExperience/UpdateWorkExperience.cshtml.Tests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Product.WorkExperience;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+namespace UnitTests.Pages.Product.Education
+{
+    class UpdateWorkExperienceTests
+    {
+        #region TestSetup
+        // UpdateWorkExperience page to test
+        public static UpdateWorkExperienceModel pageModel;
+
+        /// <summary>
+        /// Initialization for the tests to be conducted
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new UpdateWorkExperienceModel(TestHelper.ProductService)
+            {
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// Valid OnGet should return an valid WorkExperience record
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_An_WorkExperience_Record()
+        {
+            // Arrange
+
+            // Act
+            var Product = TestHelper.ProductService.GetAllData().First();
+            var WorkExperience = Product.WorkExperienceList[0];
+            // Act
+            pageModel.OnGet(WorkExperience.ProductID, WorkExperience.ID);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(WorkExperience.Title, pageModel.WorkExperience.Title);
+        }
+
+        /// <summary>
+        /// Invalid OnGet should go to to Index Page
+        /// </summary>
+        [Test]
+        public void OnGet_Product_NotValid_Should_Go_To_Index_Page()
+        {
+            // Arrange 11122 is a non_existing Product ID
+            string ProductID = "11122";
+
+            // Act
+            pageModel.OnGet(ProductID, "11122");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, pageModel.WorkExperience);
+        }
+        #endregion OnGet
+
+
+        #region OnPost
+
+        /// <summary>
+        /// Invalid OnPost with valid model should return to Update page
+        /// </summary>
+        [Test]
+        public void OnPost_Valid_Should_Redirect_to_Update()
+        {
+            // Arrange
+            var Product = TestHelper.ProductService.GetAllData().First();
+            pageModel.WorkExperience = Product.WorkExperienceList[0];
+
+            // Act
+            // Expected result for runnning OnPost
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, result.PageName.Contains("Update"));
+        }
+
+        /// <summary>
+        /// Invalid OnPost with invalid model should return the page
+        /// </summary>
+        [Test]
+        public void OnPost_InValid_Model_NotValid_Should_Return_Page()
+        {
+            // Arrange
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            // Expected result from running the OnPost
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+
+        /// <summary>
+        /// Invalid OnPost with invalid model should return the Index page
+        /// </summary>
+        [Test]
+        public void OnPost_InValid_Award_Should_Return_index_Page()
+        {
+            // Arrange
+            pageModel.WorkExperience = new WorkExperienceModel
+            {
+                ProductID = "11122", // 11122 is a non_existing Product ID
+                ID = System.Guid.NewGuid().ToString()
+            };
+
+            // Act
+            // Expected result from running the OnPost
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        #endregion OnPost
+    }
+}
