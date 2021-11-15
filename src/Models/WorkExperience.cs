05c1ad2fbed0f5d05bf418ed1352fc757fd109e6
June <june.liao.bashpole@gmail.com>
2021-11-15 03:50:45 -0800

    Add WorkExperienceModel in WorkExperience.cs

---
+++ src/Models/WorkExperience.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class WorkExperienceModel
+    {
+        // getting the work title for the work experience
+        public string Title { get; set; }
+        // getting the employer for the work experience
+        public string Employer { get; set; }
+        // getting the starting date for the work experience
+        public string StartDate { get; set; }
+        // getting the ending date for the work experience
+        public string EndDate { get; set; }
+        // getting the description for the work experience
+        public string Description { get; set; }
+    }
+}
