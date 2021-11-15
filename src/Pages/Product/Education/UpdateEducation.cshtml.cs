404f44b13d96a6347bb0c9985c1d4cd8023886af
June <june.liao.bashpole@gmail.com>
2021-11-15 03:09:15 -0800

    Redirect to Update page after update Education in UpdateEducation.cshtml.cs

--- src/Pages/Product/Education/UpdateEducation.cshtml.cs
+++ src/Pages/Product/Education/UpdateEducation.cshtml.cs
@@ -91,9 +91,9 @@ namespace ContosoCrafts.WebSite.Pages.Product.Education
 
             // Update this Product in Json 
             ProductService.UpdateData(Product);
-   
-            // Redirect to Product/Index page
-            return RedirectToPage("/Product/Index");
+
+            // Redirect to Product/update page
+            return RedirectToPage("/Product/Update", new { Id = Product.Id });
         }
     }
 }
