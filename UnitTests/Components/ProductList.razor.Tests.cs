0971475c05ce97075cbb9086b56992096fa00942
June <june.liao.bashpole@gmail.com>
2021-11-16 19:34:29 -0800

    Add UT for Razor Page in ProductList.razor.Tests.cs

---
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -0,0 +1,14 @@
+﻿using Bunit;
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Components;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Components
+{
+    class ProductList
+    {
+    }
+}
