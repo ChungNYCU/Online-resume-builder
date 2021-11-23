46ad272650a8bdd0a5349274ecb302c4b7883751
Xinyu Qiu <qiux1@seattleu.edu>
2021-11-23 14:18:26 -0800

    Add input restriction  for Work Experience model vaiables in WorkExperience.cs

--- src/Models/WorkExperience.cs
+++ src/Models/WorkExperience.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -12,14 +13,17 @@ namespace ContosoCrafts.WebSite.Models
         // This is WorkExperience ID so that we know where it is in Product.AwardList
         public string ID { get; set; }
         // getting the work title for the work experience
+        [StringLength(20, MinimumLength = 3)]
         public string Title { get; set; }
         // getting the employer for the work experience
+        [StringLength(20, MinimumLength = 3)]
         public string Employer { get; set; }
         // getting the starting date for the work experience
         public string StartDate { get; set; }
         // getting the ending date for the work experience
         public string EndDate { get; set; }
         // getting the description for the work experience
+        [StringLength(120, MinimumLength = 3)]
         public string Description { get; set; }
     }
 }
