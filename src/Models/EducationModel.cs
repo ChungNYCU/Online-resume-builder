9f8000749a41c694259b9dbc1c330be54c43ae4e
june.liao <june.liao.bashpole@gmail.com>
2021-11-24 23:02:23 -0800

    Add ErrorMessage in RegularExpression for EducationModel.cs

--- src/Models/EducationModel.cs
+++ src/Models/EducationModel.cs
@@ -15,31 +15,41 @@ namespace ContosoCrafts.WebSite.Models
         public string ProductID { get; set; }
         // This is Education ID so that we know where it is in Product.EducationList
         public string ID { get; set; }
+
         // getting the university 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         [StringLength(20, MinimumLength = 3)]
         [Required]
         public string University { get; set; }
+
         // getting the university location 
-        [StringLength(20, MinimumLength = 3)]
+        [StringLength(20, MinimumLength = 2)]
         [Required]
         public string Location { get; set; }
+
         // getting the degree earned in university
-        [StringLength(20, MinimumLength = 3)]
+        [StringLength(20, MinimumLength = 2)]
         [Required]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         public string Degree { get; set; }
+
         // getting the major studied in university
-        [StringLength(20, MinimumLength = 3)]
         [Required]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         public string Major { get; set; }
         // getting the GPA
+
         [Required]
+        [StringLength(3)]
+        [RegularExpression(@"^[0-4]{1}.[0-9]{1}$", ErrorMessage = "* Input number from 0.0 - 4.0")]
         public string GPA { get; set; }
+
         // getting the starting date for the university
         [Required]
         public string StartDate { get; set; }
         // getting  the ending date for the university
+
         [Required]
         public string EndDate { get; set; }
     }
