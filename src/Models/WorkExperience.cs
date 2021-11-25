4865c8696ecf323b6dbd47fec1b9a0da9b133920
june.liao <june.liao.bashpole@gmail.com>
2021-11-24 23:02:54 -0800

    Add ErrorMessage in RegularExpression for WorkExperienceModel.cs

--- src/Models/WorkExperience.cs
+++ src/Models/WorkExperience.cs
@@ -13,14 +13,19 @@ namespace ContosoCrafts.WebSite.Models
         // This is WorkExperience ID so that we know where it is in Product.AwardList
         public string ID { get; set; }
         // getting the work title for the work experience
+
         [Required]
-        [StringLength(20, MinimumLength = 3)]
+        [StringLength(30, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         public string Title { get; set; }
+
         // getting the employer for the work experience
         [Required]
-        [StringLength(20, MinimumLength = 3)]
+        [StringLength(30, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         public string Employer { get; set; }
         // getting the starting date for the work experience
+
         [Required]
         public string StartDate { get; set; }
         // getting the ending date for the work experience
@@ -28,7 +33,7 @@ namespace ContosoCrafts.WebSite.Models
         public string EndDate { get; set; }
         // getting the description for the work experience
         [Required]
-        [StringLength(140, MinimumLength = 3)]
+        [StringLength(800, MinimumLength = 3)]
         public string Description { get; set; }
     }
 }
