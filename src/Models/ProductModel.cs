6a7c18d54a6b7b185e0cd31160fa6b6da4f3a287
june.liao <june.liao.bashpole@gmail.com>
2021-11-24 23:05:08 -0800

    Add photo_url validation and linkedin_url validation in ProductModel

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -15,40 +15,51 @@ namespace ContosoCrafts.WebSite.Models
     {
         // getting resumes id. Id is identification for resume
         public string Id { get; set; }
-        // getting resume maker
-        public string Maker { get; set; }
+
         // pulling resume from json
         [JsonPropertyName("img")]
         // getting photo from ID in json
+        [RegularExpression(@"^((http|https)://)[a-zA-Z0-9\/_:@=.+?,#%&~-]{2,400}", ErrorMessage = "* Enter a valid photo http URL ")]
         [Required]
         public string Photo { get; set; }
+
         // getting Linkedin data
+        [RegularExpression(@"^(https://www.linkedin.com/in/)[a-zA-Z0-9\/_:@=.+?,#%&~-]{1,300}", ErrorMessage = "* Enter your Linkedin profile URL ")]
         [Required]
         public string LinkedinUrl { get; set; }
+
         // getting user name
-        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "* Only English letters allowed")]
         [Required]
         [StringLength(30)]
         public string FullName { get; set; }
+
         // getting user awards 
         public List<AwardModel> AwardList { get; set; } = new List<AwardModel>();
+
         // getting resume education history 
         public List<EducationModel> EducationList { get; set; } = new List<EducationModel>();
+
         // getting resume Work Experiences
         public List<WorkExperienceModel> WorkExperienceList { get; set; } = new List<WorkExperienceModel>();
+        
         // getting resume personal skill 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(150, MinimumLength = 3)]
         [Required]
         public string PersonalSkill { get; set; }
+
         // getting resume about me 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(150, MinimumLength = 3)]
         [Required]
         public string AboutMe { get; set; }
+
         // getting resume rating 
         public int[] Ratings { get; set; }
+
         // get user password 
         [Required]
         public string Password { get; set; }
+
         // get Viewed
         public int Viewed { get; set; } = 0;
     }
